f6cc2bffebe8e88b4908bb17c5832e5b3c08e5c8
ankitdoyiimn <p21ankitn@iimnagpur.ac.in>
2026-09-05 19:27:50 +0530

    Added Chains Code

---
+++ 7.Chains/simple_chain.py
@@ -0,0 +1,24 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+# Define the model
+model = ChatOpenAI()
+
+prompt = PromptTemplate(
+    template= "Generate 5 interesting facts about {topic}",
+    input_variables=['topic']
+)
+
+parser = StrOutputParser()
+
+chain= prompt | model | parser
+
+result = chain.invoke({'topic':'Sri Lanka'})
+
+print(result)
+
+chain.get_graph().print_ascii
